a146239b082bfec029b88ab35173b647fcc4afbe
stand4rd <thomas.noble.jordan@gmail.com>
2023-10-19 11:34:46 -0700

    updated base code

--- custom_blocks.js
+++ custom_blocks.js
@@ -1,114 +1,73 @@
-//const makeHttpRequest = require('./http_request');
-//const url = 'localhost:5500/';
-// makeHttpRequest(url); -> supply this into blockly button 
-
-Blockly.Blocks['printTest'] = {
-    init: function() {
-        this.appendDummyInput()
-        .appendField("Print 'test' to screen");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(160);
-        this.setTooltip("");
-        this.setHelpUrl("");
-    }
-};
-    
-Blockly.JavaScript['printTest'] = function(block) {
-    // Generate JavaScript code to print "test" to the screen
-    var code = 'console.log("test");\n';
-    return code;
-};
-
-// Define a custom block for "Delete Test" button.
-Blockly.Blocks['deleteTest'] = {
-    init: function () {
-        this.appendDummyInput()
-            .appendField("Delete Test");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(160);
-        this.setTooltip("Click to delete the test.");
-        this.setHelpUrl("");
-    }
-};
-
-// Define the JavaScript code generation for the custom block.
-Blockly.JavaScript['deleteTest'] = function (block) {
-    // Generate JavaScript code to perform the delete test action.
-    alert("Test deleted!");
-    var code = 'deleteTest();\n';
-    return code;
-};
-
-Blockly.Blocks['dummy_test'] = {
-    init: function () {
-        this.appendDummyInput()
-            .appendField("Dummy Test");
-        this.appendValueInput("INPUT1")
-            .setCheck(null)
-            .appendField("Input 1");
-        this.appendValueInput("INPUT2")
-            .setCheck(null)
-            .appendField("Input 2");
-        this.setInputsInline(true);
-        this.setOutput(true, null);
-        this.setColour(160);
-        this.setTooltip("This is a dummy test block with two inputs.");
-        this.setHelpUrl("");
-    }
-};
-
-Blockly.JavaScript['dummy_test'] = function (block) {
-    var value_input1 = Blockly.JavaScript.valueToCode(block, 'INPUT1', Blockly.JavaScript.ORDER_ATOMIC);
-    var value_input2 = Blockly.JavaScript.valueToCode(block, 'INPUT2', Blockly.JavaScript.ORDER_ATOMIC);
-
-    // You can use value_input1 and value_input2 in your JavaScript code.
-    // For demonstration, we'll concatenate the inputs and return them.
-    var code = value_input1 + " " + value_input2;
-    return [code, Blockly.JavaScript.ORDER_ATOMIC];
-};
-
-// Define a custom Blockly block for generating JSON for an HTTP request.
 Blockly.Blocks['generateHttpRequestJson'] = {
     init: function () {
         this.appendDummyInput()
-            .appendField("Generate HTTP Request JSON");
+            .appendField("Generate HTTP Request");
+
         this.appendValueInput("URL")
             .setCheck("String")
+            .setAlign(Blockly.ALIGN_RIGHT)
             .appendField("URL");
+
         this.appendValueInput("Headers")
             .setCheck("Object")
+            .setAlign(Blockly.ALIGN_RIGHT)
             .appendField("Headers (JSON)");
+
         this.appendValueInput("Parameters")
             .setCheck("Object")
+            .setAlign(Blockly.ALIGN_RIGHT)
             .appendField("Parameters (JSON)");
+
         this.appendValueInput("Data")
             .setCheck("Object")
+            .setAlign(Blockly.ALIGN_RIGHT)
             .appendField("Data (JSON)");
+
+        this.appendDummyInput()
+            .setAlign(Blockly.ALIGN_RIGHT)
+            .appendField("HTTP Method")
+            .appendField(new Blockly.FieldDropdown([["GET", "GET"], ["POST", "POST"]]), "Method");
+
         this.setOutput(true, "Object");
-        this.setColour(160);
+        this.setColour(250);
         this.setTooltip("Generate JSON for an HTTP request.");
         this.setHelpUrl("");
     }
 };
 
-// Define the JavaScript code generation for the custom block.
+// Define the JavaScript code generation for the custom block using .forBlock[blockType].
 Blockly.JavaScript['generateHttpRequestJson'] = function (block) {
+    var method = block.getFieldValue('Method');
     var value_url = Blockly.JavaScript.valueToCode(block, 'URL', Blockly.JavaScript.ORDER_ATOMIC);
     var value_headers = Blockly.JavaScript.valueToCode(block, 'Headers', Blockly.JavaScript.ORDER_ATOMIC);
     var value_parameters = Blockly.JavaScript.valueToCode(block, 'Parameters', Blockly.JavaScript.ORDER_ATOMIC);
     var value_data = Blockly.JavaScript.valueToCode(block, 'Data', Blockly.JavaScript.ORDER_ATOMIC);
 
+    // Define the block type and generate code accordingly.
+    var blockType = 'generateHttpRequestJson';
+
     // Generate the JSON object for the HTTP request.
     var code = '{\n';
-    code += '  "url": ' + value_url + ',\n';
-    code += '  "headers": ' + value_headers + ',\n';
-    code += '  "parameters": ' + value_parameters + ',\n';
-    code += '  "data": ' + value_data + '\n';
-    code += '}\n';
+    if (value_url) {
+        code += '  "url": ' + value_url + ',\n';
+    }
+    if (value_headers) {
+        code += '  "headers": ' + value_headers + ',\n';
+    }
+    if (value_parameters) {
+        code += '  "parameters": ' + value_parameters + ',\n';
+    }
+    if (value_data) {
+        code += '  "data": ' + value_data + ',\n';
+    }
+    code += '  "method": "' + method + '"\n';
+    code += '}';
 
-    return [code, Blockly.JavaScript.ORDER_ATOMIC];
+    // Use the .forBlock[blockType] dictionary for code generation.
+    var generator = Blockly.getMainWorkspace().getGenerator();
+    var codeForBlock = generator['.forBlock'][blockType];
+    console.log(code)
+    return codeForBlock(block);
 };
 
 Blockly.Blocks['http_request_url'] = {
@@ -117,7 +76,7 @@ Blockly.Blocks['http_request_url'] = {
             .appendField("URL")
             .appendField(new Blockly.FieldTextInput("https://example.com"), "URL");
         this.setOutput(true, "String");
-        this.setColour(160);
+        this.setColour(100);
         this.setTooltip("Input for the URL of the HTTP request.");
         this.setHelpUrl("");
     }
@@ -134,7 +93,7 @@ Blockly.Blocks['http_request_headers'] = {
             .appendField("Headers (JSON)")
             .appendField(new Blockly.FieldTextInput('{"Content-Type": "application/json"}'), "Headers");
         this.setOutput(true, "Object");
-        this.setColour(160);
+        this.setColour(200);
         this.setTooltip("Input for the headers (JSON) of the HTTP request.");
         this.setHelpUrl("");
     }
@@ -157,7 +116,7 @@ Blockly.Blocks['http_request_parameters'] = {
             .appendField("Parameters (JSON)")
             .appendField(new Blockly.FieldTextInput('{"param1": "value1", "param2": "value2"}'), "Parameters");
         this.setOutput(true, "Object");
-        this.setColour(160);
+        this.setColour(300);
         this.setTooltip("Input for the parameters (JSON) of the HTTP request.");
         this.setHelpUrl("");
     }
@@ -180,7 +139,7 @@ Blockly.Blocks['http_request_data'] = {
             .appendField("Data (JSON)")
             .appendField(new Blockly.FieldTextInput('{"key1": "value1", "key2": "value2"}'), "Data");
         this.setOutput(true, "Object");
-        this.setColour(160);
+        this.setColour(0);
         this.setTooltip("Input for the data (JSON) of the HTTP request.");
         this.setHelpUrl("");
     }
@@ -195,4 +154,24 @@ Blockly.JavaScript['http_request_data'] = function (block) {
         console.error("Invalid JSON format for data: " + data);
         return ["{}", Blockly.JavaScript.ORDER_ATOMIC];
     }
+};
+
+// Define a custom Blockly block for selecting the HTTP method.
+Blockly.Blocks['http_request_method'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("HTTP Method");
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldDropdown([["GET", "GET"], ["POST", "POST"]]), "METHOD");
+        this.setOutput(true, "String");
+        this.setColour(55);
+        this.setTooltip("Select the HTTP method (GET or POST).");
+        this.setHelpUrl("");
+    }
+};
+
+// Define the JavaScript code generation for the custom block.
+Blockly.JavaScript['http_request_method'] = function (block) {
+    var method = block.getFieldValue('METHOD');
+    return [method, Blockly.JavaScript.ORDER_ATOMIC];
 };
