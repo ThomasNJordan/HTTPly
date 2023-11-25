1664c326a95ef2ea07f273b6afc5eaa99fbc3197
stand4rd <thomas.noble.jordan@gmail.com>
2023-11-24 18:33:03 -0800

    Added parsing to output from response

--- src/custom_blocks.js
+++ src/custom_blocks.js
@@ -18,11 +18,6 @@ Blockly.Blocks['generateHttpRequestJson'] = {
             .setAlign(Blockly.ALIGN_RIGHT)
             .appendField("Headers (JSON)");
 
-        this.appendValueInput("Parameters")
-            .setCheck("Object")
-            .setAlign(Blockly.ALIGN_RIGHT)
-            .appendField("Parameters (JSON)");
-
         this.appendValueInput("Data")
             .setCheck("Object")
             .setAlign(Blockly.ALIGN_RIGHT)
@@ -40,7 +35,6 @@ Blockly.JavaScript['generateHttpRequestJson'] = function (block) {
     var method = block.getFieldValue('Method');
     var value_url = Blockly.JavaScript.valueToCode(block, 'URL', Blockly.JavaScript.ORDER_ATOMIC);
     var value_headers = Blockly.JavaScript.valueToCode(block, 'Headers', Blockly.JavaScript.ORDER_ATOMIC);
-    var value_parameters = Blockly.JavaScript.valueToCode(block, 'Parameters', Blockly.JavaScript.ORDER_ATOMIC);
     var value_data = Blockly.JavaScript.valueToCode(block, 'Data', Blockly.JavaScript.ORDER_ATOMIC);
 
     // Generate the JSON object for the HTTP request.
@@ -50,9 +44,6 @@ Blockly.JavaScript['generateHttpRequestJson'] = function (block) {
     }
     if (value_headers) {
         code += '  "headers": ' + value_headers + ',\n';
-    }
-    if (value_parameters) {
-        code += '  "parameters": ' + value_parameters + ',\n';
     }
     if (value_data) {
         code += '  "data": ' + value_data + ',\n';
@@ -86,7 +77,7 @@ Blockly.Blocks['http_request_headers'] = {
             .appendField("Headers (JSON)")
             .appendField(new Blockly.FieldTextInput('{"Content-Type": "application/json"}'), "Headers");
         this.setOutput(true, "Object");
-        this.setColour(230);
+        this.setColour("rgb(234, 67, 53)"); 
         this.setTooltip("Input for the headers (JSON) of the HTTP request.");
         this.setHelpUrl("");
     }
@@ -104,35 +95,11 @@ Blockly.JavaScript['http_request_headers'] = function(block) {
     return [headersCode, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
-
-Blockly.Blocks['http_request_parameters'] = {
-    init: function () {
-        this.appendDummyInput()
-            .appendField("Parameters (JSON)")
-            .appendField(new Blockly.FieldTextInput('{"param1": "value1", "param2": "value2"}'), "Parameters");
-        this.setOutput(true, "Object");
-        this.setColour("rgb(251, 188, 5)"); 
-        this.setTooltip("Input for the parameters (JSON) of the HTTP request.");
-        this.setHelpUrl("");
-    }
-};
-
-Blockly.JavaScript['http_request_parameters'] = function (block) {
-    var parameters = block.getFieldValue('Parameters');
-    try {
-        var parsedParameters = JSON.parse(parameters);
-        return [JSON.stringify(parsedParameters), Blockly.JavaScript.ORDER_ATOMIC];
-    } catch (e) {
-        console.error("Invalid JSON format for parameters: " + parameters);
-        return ["{}", Blockly.JavaScript.ORDER_ATOMIC];
-    }
-};
-
 Blockly.Blocks['http_request_data'] = {
     init: function () {
         this.appendDummyInput()
             .appendField("Data (JSON)")
-            .appendField(new Blockly.FieldTextInput('{"key1": "value1", "key2": "value2"}'), "Data");
+            .appendField(new Blockly.FieldTextInput('{"param1": "value1", "param2": "value2"}'), "Data");
         this.setOutput(true, "Object");
         this.setColour("rgb(142, 36, 170)");
         this.setTooltip("Input for the data (JSON) of the HTTP request.");
